02da9731ea487e3cdd40d51baefbdbe39e5f3edc
venubhat23 <venubha147@gmail.com>
2026-04-14 08:38:47 +0530

    database Updatd

--- database.py
+++ database.py
@@ -1,9 +1,16 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv   # <-- you need this import
+
+# Load environment variables from .env file
+load_dotenv()
 
 def get_db():
     return mysql.connector.connect(
-        host="127.0.0.1",
-        database="users_db",
-        user="root",
-        password="root"
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
     )
+
+print(os.getenv("DB_HOST"))
